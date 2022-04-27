a7e75cc18cfc09dbf05c67778079b4f05388780f
Rigo Galicia <rigo.galicia@icloud.com>
2022-04-27 13:54:56 -0600

    Se creo la funcion lambda para crear una tarea

---
+++ src/create-task.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const { v4 } = require('uuid');
+const AWS = require('aws-sdk');
+
+const createTask = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  const { title, description } = JSON.parse(event.body);
+  const createdAt = new Date();
+  const id = v4();
+
+  const newTask = {
+    id,
+    title,
+    description,
+    createdAt
+  }
+
+  await dynamodb.put({
+    TableName: 'TaskTable',
+    Item: newTask
+  }).promise();
+
+
+  return {
+    statusCode: 201,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newTask)
+  }
+
+};
+
+module.exports = {
+  createTask
+};
